5328f4f7ae8e4031a6fe48f05ba6df6536d1c4f6
ごとうちひろ <leedsroccaguaita@gmail.com>
2020-06-01 18:38:56 +0900

    added shade

--- ShowObj/main.cpp
+++ ShowObj/main.cpp
@@ -6,9 +6,15 @@
 #include <GLFW/glfw3.h>
 #include <cstdlib>
 #include <cstdio>
+#define STB_IMAGE_IMPLEMENTATION
 
 std::vector<double> v;
-std::vector<unsigned int> f;
+std::vector<double> vn;
+std::vector<double> vt;
+std::vector<unsigned int> f_v;
+std::vector<unsigned int> f_t;
+std::vector<unsigned int> f_n;
+
 
 
 
@@ -26,24 +32,72 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 
 
-int strip_slash(std::string s){
+std::vector<unsigned int> strip_slash(std::string s){
     
-    std::vector<int> digits;
-    digits.clear();
-    int v_num = 0;
+    std::vector<unsigned int> vtn_values;
+    vtn_values.clear();
     
-    for (int i=0; i<s.size(); i++){
-        if (s[i] != '/'){
-            digits.push_back(s[i]);
-        }
-        else break;
+    std::vector<int> digits_v;
+    std::vector<int> digits_t;
+    std::vector<int> digits_n;
+    digits_v.clear();
+    digits_n.clear();
+    digits_t.clear();
+    int v_value = 0;
+    int t_value = 0;
+    int n_value = 0;
+    int idx = 0;
+    
+    // "***/***/***"の状態の文字列から、v,t,nの各インデックス文字列に分離する
+    while (s[idx]!='/'){
+        digits_v.push_back(s[idx]);
+        idx++;
     }
-    for (int i=0; i<digits.size(); i++){
-        v_num +=
+    idx++;
+    while (s[idx]!='/'){
+        digits_t.push_back(s[idx]);
+        idx++;
+    }
+    idx++;
+    while (idx < s.size()){
+        digits_n.push_back(s[idx]);
+        idx++;
+    }
+    
+    // インデックスの文字列を10進数に変換
+    for (int i=0; i<digits_v.size(); i++){
+        v_value +=
             std::pow(10, i)
-            * (digits[digits.size() - 1 - i] - '0');
+            * (digits_v[digits_v.size() - 1 - i] - '0');
     }
-    return v_num;
+    for (int i=0; i<digits_t.size(); i++){
+        t_value +=
+            std::pow(10, i)
+            * (digits_t[digits_t.size() - 1 - i] - '0');
+    }
+    for (int i=0; i<digits_n.size(); i++){
+        n_value +=
+            std::pow(10, i)
+            * (digits_n[digits_n.size() - 1 - i] - '0');
+    }
+    
+    vtn_values.push_back(v_value);
+    vtn_values.push_back(t_value);
+    vtn_values.push_back(n_value);
+    
+    /*
+    for (int i=0; i<s.size(); i++){
+        std::cout << s[i];
+    }
+    std::cout << std::endl;
+    std::cout << "s.size(): " << s.size() << std::endl;
+    std::cout << "v: " << v_value << std::endl;
+    std::cout << "t: " << t_value << std::endl;
+    std::cout << "n: " << n_value << std::endl;
+    std::cout << std::endl;
+     */
+    
+    return vtn_values;
 }
 
 void read_obj(std::string objfile_name){
@@ -54,7 +108,11 @@ void read_obj(std::string objfile_name){
     if (fin == NULL) std::cout << "File Open Failed" << std::endl;
     
     v.clear();
-    f.clear();
+    vn.clear();
+    vt.clear();
+    f_v.clear();
+    f_t.clear();
+    f_n.clear();
     
     const int BUFF_SIZE = 256;
     char buff[BUFF_SIZE];
@@ -62,6 +120,7 @@ void read_obj(std::string objfile_name){
     while(!fin.eof()){
         fin.getline(buff, BUFF_SIZE);
         
+        // 先頭が'v'の場合
         if (buff[0]=='v' && buff[1]==' '){
             char str[256];
             double x, y, z;
@@ -70,24 +129,62 @@ void read_obj(std::string objfile_name){
             v.push_back(y);
             v.push_back(z);
         }
+        // 先頭が'vn'の場合
+        if (buff[0]=='v' && buff[1]=='n'){
+            char str[256];
+            double x, y, z;
+            sscanf(buff, "%s %lf %lf %lf", str, &x, &y, &z);
+            vn.push_back(x);
+            vn.push_back(y);
+            vn.push_back(z);
+        }
+        // 先頭が'vt'の場合
+        if (buff[0]=='v' && buff[1]=='t'){
+            char str[256];
+            double x, y, z;
+            sscanf(buff, "%s %lf %lf %lf", str, &x, &y, &z);
+            vt.push_back(x);
+            vt.push_back(y);
+            vt.push_back(z);
+        }
+        // 先頭が'f'の場合
         if (buff[0]=='f'){
             char str[256];
             char i0[256], i1[256], i2[256], i3[256]={'\0'};
             sscanf(buff, "%s %s %s %s %s", str, i0, i1, i2, i3);
-            int v0, v1, v2, v3;
-            v0 = strip_slash(i0);
-            v1 = strip_slash(i1);
-            v2 = strip_slash(i2);
-            f.push_back(v0-1);
-            f.push_back(v1-1);
-            f.push_back(v2-1);
+            std::vector<unsigned int> vertex0, vertex1, vertex2;
+            vertex0.clear();
+            vertex1.clear();
+            vertex2.clear();
+            vertex0 = strip_slash(i0);
+            vertex1 = strip_slash(i1);
+            vertex2 = strip_slash(i2);
+            f_v.push_back(vertex0[0]-1);
+            f_v.push_back(vertex1[0]-1);
+            f_v.push_back(vertex2[0]-1);
+            f_t.push_back(vertex0[1]-1);
+            f_t.push_back(vertex1[1]-1);
+            f_t.push_back(vertex2[1]-1);
+            f_n.push_back(vertex0[2]-1);
+            f_n.push_back(vertex1[2]-1);
+            f_n.push_back(vertex2[2]-1);
+            // 四角形メッシュを加える場合、三角形に分割
             if (i3[0]!='\0'){
-                v3 = strip_slash(i3);
-                f.push_back(v0-1);
-                f.push_back(v2-1);
-                f.push_back(v3-1);
+                std::vector<unsigned int> vertex3;
+                vertex3.clear();
+                vertex3 = strip_slash(i3);
+                f_v.push_back(vertex0[0]-1);
+                f_v.push_back(vertex2[0]-1);
+                f_v.push_back(vertex3[0]-1);
+                f_t.push_back(vertex0[1]-1);
+                f_t.push_back(vertex2[1]-1);
+                f_t.push_back(vertex3[1]-1);
+                f_n.push_back(vertex0[2]-1);
+                f_n.push_back(vertex2[2]-1);
+                f_n.push_back(vertex3[2]-1);
             }
         }
+        
     }
     
     fin.close();
@@ -108,11 +205,18 @@ void display(void){
   glColor3f(0.5f, 0.5f, 0.0f);
   glBegin(GL_TRIANGLES);
     
-  for (int i = 0; i < f.size(); i++) {
+  for (int i = 0; i < f_v.size(); i++) {
+      
+    double n_vec[3] = {
+    vn[f_n[i] * 3 + 0],
+    vn[f_n[i] * 3 + 1],
+    vn[f_n[i] * 3 + 2]};
+    glNormal3dv(n_vec);
+      
     float x, y, z;
-    x = v[f[i] * 3 + 0];
-    y = v[f[i] * 3 + 1];
-    z = v[f[i] * 3 + 2];
+    x = v[f_v[i] * 3 + 0];
+    y = v[f_v[i] * 3 + 1];
+    z = v[f_v[i] * 3 + 2];
     glVertex3f(x, y, z);
   }
   glEnd();
@@ -139,6 +243,10 @@ int main(void)
   std::cin >> objfile_name;
   read_obj(objfile_name);
     
+  /*for (int i=0; i<vt.size()/3; i++){
+      std::cout << "vt: " << vt[3*i] << ", " << vt[3*i+1] << ", " << vt[3*i+2] << std::endl;
+  }*/
+    
   // GLFWを初期化し、ウィンドウを作成
   if (!glfwInit())
     exit(EXIT_FAILURE);
@@ -164,6 +272,7 @@ int main(void)
     ratio = width / (float) height;
     glViewport(0, 0, width, height);
     glClear(GL_COLOR_BUFFER_BIT);
+    
       
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -175,7 +284,54 @@ int main(void)
     glTranslatef(0.0f, -0.5f, 0.0f);
       
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+      
+    // テクスチャの設定
+      /*
+    #define TEXWIDTH  256
+    #define TEXHEIGHT 256
+    static const char texture1[] = "bug.jpg";
+    GLubyte texture[TEXHEIGHT][TEXWIDTH][3];
+    FILE *fp;
     
+    // テクスチャ画像の読み込み
+    if ((fp = fopen(texture1, "rb")) != NULL){
+        fread(texture, sizeof(texture), 1, fp);
+        fclose(fp);
+    }
+    else {
+        std::cout << "Texture file not opened" << std::endl;
+    }
+      
+    // メモリ上に画素情報がどのように格納されているかをGPUに伝える(1->画素境界のバイト数)
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    // テクスチャの割り当て
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, TEXWIDTH, TEXHEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, texture);
+      
+    // テクスチャを拡大・縮小する方法を指定
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+      
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+      
+    glEnable(GL_TEXTURE_2D);
+
+    int TEXSIZE = 4;
+    glTexImage2D(
+        GL_TEXTURE_2D , 0 , 3 , TEXSIZE , TEXSIZE ,
+        0 , GL_RGB , GL_UNSIGNED_BYTE , texture
+    );
+    glBindTexture(GL_TEXTURE_2D, 0);
+      */
+    // 光源の初期設定 (光源の位置, 直接光強度, 環境光強度)
+    const GLfloat lightPos[] = {3, 0, -2, 1};
+    const GLfloat lightCol[] = {1, 1, 0, 1};
+    const GLfloat lightAmb[] = {0.1, 0.1, 0.1, 1.0};
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightCol);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmb);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    //glEnable(GL_DEPTH_TEST);
     display();
     // カラーバッファを入れ替える
     glfwSwapBuffers(window);

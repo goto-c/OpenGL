b465987cf4b64d6fc70b504f643e0efe4b9e9c58
ごとうちひろ <leedsroccaguaita@gmail.com>
2020-07-16 10:21:09 +0900

    functionize shader

--- 04_ShowObjWithShader/main.cpp
+++ 04_ShowObjWithShader/main.cpp
@@ -87,6 +87,94 @@ void display(const std::vector<double>& aXYZ,
       glEnd();
   }
 }
+
+std::string read_shader_file(std::string &f)
+{
+    std::string source;
+    source.clear();
+
+    std::ifstream fin;
+    fin.open(f);
+    if(fin.fail())
+        std::cout << "shader source file open failed" << std::endl;
+    
+    const int BUFF_SIZE = 1024;
+    char buff[BUFF_SIZE];
+    
+    while (!fin.eof()){
+        fin.getline(buff, BUFF_SIZE);
+        source += buff;
+        source += "\n";
+    }
+    source.pop_back();
+    source += "\0";
+
+    return source;
+}
+
+int use_shader(std::string &vrt_path,
+               std::string &frg_path)
+{
+    std::string sglslvrt = read_shader_file(vrt_path);
+    std::string sglslfrg = read_shader_file(frg_path);
+    const char *glslvrt = sglslvrt.c_str();
+    const char *glslfrg = sglslfrg.c_str();
+    
+    std::cout << "read vrt_shader: " << std::endl;
+    std::cout << read_shader_file(vrt_path) << std::endl;
+    std::cout << "read frg_shader: " << std::endl;
+    std::cout << read_shader_file(frg_path) << std::endl;
+    
+    if (glslvrt == NULL){
+        std::cout << "Could not read file: " << vrt_path << std::endl;
+        return -1;
+    }
+    
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &glslvrt, nullptr);
+    glCompileShader(vertexShader);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << infoLog << std::endl;
+    }
+    
+    if (glslfrg == NULL){
+        std::cout << "Could not read file: " << frg_path << std::endl;
+        return -1;
+    }
+    
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &glslfrg, nullptr);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << infoLog << std::endl;
+    }
+    
+    // link shader
+    shaderProgram = glCreateProgram(); // プログラムのIDを取得
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success){
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKAGE_FAILED" << infoLog << std::endl;
+    }
+    
+    std::cout << vertexShader << " " << fragmentShader << " shader program: " << shaderProgram << std::endl;
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    glUseProgram(shaderProgram);
+    return -1;
+}
  
 /* ------------------ main --------------------*/
 
@@ -152,7 +240,7 @@ int main(void)
   if (!glfwInit())
     exit(EXIT_FAILURE);
     
-    // 新しいものを使う場合、majorを4とかにする。
+    // 新しいものを使う場合、majorを4などにする。
 //  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
 //  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 //  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -175,66 +263,9 @@ int main(void)
   }
   
   {
-      // glsl version 120
-      // vertex shader
-      const char *glslvrt =
-      "#version 120\n"
-      "varying vec3 normal;\n"
-      "void main()\n"
-      "{\n"
-      "   gl_Position = ftransform();\n"
-      "   normal = vec3(gl_NormalMatrix    * gl_Normal);\n"
-      "}\0";
-      // gl_NormalMatrixは回転行列
-      // ftransform()glVertexなどのコマンドで送った情報に回転行列をかけたもの
-      
-      // fragment shader
-      const char *glslfrg =
-      "#version 120\n"
-      "varying vec3 normal;\n"
-      "void main()\n"
-      "{\n"
-      "   gl_FragColor = vec4(0.5*normal+0.5,1);\n"
-      "}\n\0";
-      
-      // vertex shader
-      int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-      glShaderSource(vertexShader, 1, &glslvrt, nullptr);
-      glCompileShader(vertexShader);
-      int success;
-      char infoLog[512];
-      glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-      if (!success)
-      {
-          glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-          std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << infoLog << std::endl;
-      }
-      
-      // fragment shader
-      int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-      glShaderSource(fragmentShader, 1, &glslfrg, nullptr);
-      glCompileShader(fragmentShader);
-      glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-      if (!success)
-      {
-          glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-          std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << infoLog << std::endl;
-      }
-      
-      // link shader
-      shaderProgram = glCreateProgram(); //プログラムのIDを取得
-      glAttachShader(shaderProgram, vertexShader);
-      glAttachShader(shaderProgram, fragmentShader);
-      glLinkProgram(shaderProgram);
-      glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-      if(!success){
-          glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-          std::cout << "ERROR::SHADER::PRORAM::LINKAGE_FAILED" << infoLog << std::endl;
-      }
-      std::cout << fragmentShader << " " << vertexShader << " shader program: " << shaderProgram << std::endl;
-      glDeleteShader(vertexShader);
-      glDeleteShader(fragmentShader);
-    
+      std::string vrt_path = std::string(PATH_ROOT_DIR) + "/test_data/normalmap.vert";
+      std::string frg_path = std::string(PATH_ROOT_DIR) + "/test_data/normalmap.frag";
+      use_shader(vrt_path, frg_path);
   }
         
   
@@ -305,7 +336,7 @@ int main(void)
     glRotatef(90, 0.0, 1.0, 0.0);
     glTranslatef(0.0f, -0.5f, 0.0f);
       
-    glUseProgram(shaderProgram);
+    //glUseProgram(shaderProgram);
       //この後のglBegin, glEndには全てシェーダが適用される。0番を指定した場合は、デフォルトのシェーダを適用
 //    glBindVertexArray(VAO);
 //    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_Tri);

36f0daa9cf5bceffb6579f3a521f6c096edff742
goto-c <leedsroccaguaita@gmail.com>
2020-11-30 14:53:32 +0900

    add ;

--- src/objloader.h
+++ src/objloader.h
@@ -37,10 +37,10 @@ const std::string& file_obj);
 
 void read_obj(std::vector<double>& aXYZ,
               std::vector<unsigned int>& aTri_XYZ,
-              const std::string& file_obj)
+              const std::string& file_obj);
 
 void read_flag(std::vector<unsigned int>& flag,
-               const std::string& file_flag)
+               const std::string& file_flag);
 
 void read_mtl(std::vector<CMaterialInfo>& aMat,
 const std::string& file_mtl);

b23b91c55cbb75a3f77ee58d27763ac8876ddf12
goto-c <gotochihiro47@gmail.com>
2023-06-18 19:05:00 +0900

    format

--- src/shader.cpp
+++ src/shader.cpp
@@ -1,106 +1,100 @@
-
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
 #include <glad/glad.h>
-#include <GLFW/glfw3.h>
 
 #include "shader.h"
 
-std::string read_shader_file(std::string &f)
-{
+std::string read_shader_file(std::string &f) {
     std::string source;
     source.clear();
 
     std::ifstream fin;
     fin.open(f);
-    if(fin.fail())
+    if (fin.fail())
         std::cout << "shader source file open failed" << std::endl;
-    
+
     const int BUFF_SIZE = 1024;
     char buff[BUFF_SIZE];
-    
-    while (!fin.eof()){
+
+    while (!fin.eof()) {
         fin.getline(buff, BUFF_SIZE);
         source += buff;
         source += "\n";
     }
     source.pop_back();
     source += "\0";
-    
+
     fin.close();
 
     return source;
 }
 
-int use_shader(std::string &vrt_path,
-               std::string &frg_path,
-               std::vector<float> aXYZ,
-               std::vector<float> aNrm,
-               std::vector<float> aTex,
-               std::vector<unsigned int> aTri_XYZ)
-{
+int use_shader(std::string &vrt_path, std::string &frg_path,
+               std::vector<float> aXYZ, std::vector<float> aNrm,
+               std::vector<float> aTex, std::vector<unsigned int> aTri_XYZ) {
 
     std::string sglslvrt = read_shader_file(vrt_path);
     std::string sglslfrg = read_shader_file(frg_path);
     const char *glslvrt = sglslvrt.c_str();
     const char *glslfrg = sglslfrg.c_str();
-    
+
     std::cout << "read vrt_shader: " << std::endl;
     std::cout << read_shader_file(vrt_path) << std::endl;
     std::cout << "read frg_shader: " << std::endl;
     std::cout << read_shader_file(frg_path) << std::endl;
-    
-    if (glslvrt == NULL){
+
+    if (glslvrt == NULL) {
         std::cout << "Could not read file: " << vrt_path << std::endl;
         return -1;
     }
-    
+
     int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &glslvrt, nullptr);
     glCompileShader(vertexShader);
     int success;
     char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
+    if (!success) {
         glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << infoLog
+                  << std::endl;
     }
-    
-    if (glslfrg == NULL){
+
+    if (glslfrg == NULL) {
         std::cout << "Could not read file: " << frg_path << std::endl;
         return -1;
     }
-    
+
     int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &glslfrg, nullptr);
     glCompileShader(fragmentShader);
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
+    if (!success) {
         glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << infoLog
+                  << std::endl;
     }
-    
+
     GLuint programHandle;
-     
+
     // link shader
     programHandle = glCreateProgram();
     glAttachShader(programHandle, vertexShader);
     glAttachShader(programHandle, fragmentShader);
     glLinkProgram(programHandle);
     glGetProgramiv(programHandle, GL_LINK_STATUS, &success);
-    if(!success){
+    if (!success) {
         glGetProgramInfoLog(programHandle, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKAGE_FAILED" << infoLog << std::endl;
-    }
-    else {
+        std::cout << "ERROR::SHADER::PROGRAM::LINKAGE_FAILED" << infoLog
+                  << std::endl;
+    } else {
         glUseProgram(programHandle);
     }
-    std::cout << vertexShader << " " << fragmentShader << " shader program: " << programHandle << std::endl;
+    std::cout << vertexShader << " " << fragmentShader
+              << " shader program: " << programHandle << std::endl;
 
     GLuint vaoHandle;
 
@@ -116,20 +110,26 @@ int use_shader(std::string &vrt_path,
 
     // Send data to buffer
     glBindBuffer(GL_ARRAY_BUFFER, positionBufferHandle);
-    glBufferData(GL_ARRAY_BUFFER, aXYZ.size()*sizeof(GLfloat), aXYZ.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, aXYZ.size() * sizeof(GLfloat), aXYZ.data(),
+                 GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, colorBufferHandle);
-    glBufferData(GL_ARRAY_BUFFER, color_white.size()*sizeof(GLfloat), color_white.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, color_white.size() * sizeof(GLfloat),
+                 color_white.data(), GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferHandle);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, aTri_XYZ.size()*sizeof(unsigned int), aTri_XYZ.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                 aTri_XYZ.size() * sizeof(unsigned int), aTri_XYZ.data(),
+                 GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, normalBufferHandle);
-    glBufferData(GL_ARRAY_BUFFER, aNrm.size()*sizeof(GLfloat), aNrm.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, aNrm.size() * sizeof(GLfloat), aNrm.data(),
+                 GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, textureBufferHandle);
-    glBufferData(GL_ARRAY_BUFFER, aTex.size()*sizeof(GLfloat), aTex.data(), GL_STATIC_DRAW);
-    // 
+    glBufferData(GL_ARRAY_BUFFER, aTex.size() * sizeof(GLfloat), aTex.data(),
+                 GL_STATIC_DRAW);
+    //
 
     glGenVertexArrays(1, &vaoHandle);
     glBindVertexArray(vaoHandle);
@@ -151,84 +151,83 @@ int use_shader(std::string &vrt_path,
 
     glBindBuffer(GL_ARRAY_BUFFER, normalBufferHandle);
     glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
-    
+
     glBindBuffer(GL_ARRAY_BUFFER, textureBufferHandle);
     glVertexAttribPointer(4, 2, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL);
-    // 
+    //
 
     glBindVertexArray(vaoHandle);
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
-    
+
     return programHandle;
 }
 
-int use_shader(std::string &vrt_path,
-               std::string &frg_path)
-{
+int use_shader(std::string &vrt_path, std::string &frg_path) {
     std::string sglslvrt = read_shader_file(vrt_path);
     std::string sglslfrg = read_shader_file(frg_path);
     const char *glslvrt = sglslvrt.c_str();
     const char *glslfrg = sglslfrg.c_str();
-    
+
     std::cout << "read vrt_shader: " << std::endl;
     std::cout << read_shader_file(vrt_path) << std::endl;
     std::cout << "read frg_shader: " << std::endl;
     std::cout << read_shader_file(frg_path) << std::endl;
-    
-    if (glslvrt == NULL){
+
+    if (glslvrt == NULL) {
         std::cout << "Could not read file: " << vrt_path << std::endl;
         return -1;
     }
-    
+
     int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &glslvrt, nullptr);
     glCompileShader(vertexShader);
     int success;
     char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
+    if (!success) {
         glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << infoLog
+                  << std::endl;
     }
-    
-    if (glslfrg == NULL){
+
+    if (glslfrg == NULL) {
         std::cout << "Could not read file: " << frg_path << std::endl;
         return -1;
     }
-    
+
     int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &glslfrg, nullptr);
     glCompileShader(fragmentShader);
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
+    if (!success) {
         glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << infoLog
+                  << std::endl;
     }
-    
+
     GLuint programHandle;
-     
+
     // link shader
     programHandle = glCreateProgram(); // プログラムのIDを取得
     glAttachShader(programHandle, vertexShader);
     glAttachShader(programHandle, fragmentShader);
     glLinkProgram(programHandle);
     glGetProgramiv(programHandle, GL_LINK_STATUS, &success);
-    if(!success){
+    if (!success) {
         glGetProgramInfoLog(programHandle, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKAGE_FAILED" << infoLog << std::endl;
-    }
-    else {
+        std::cout << "ERROR::SHADER::PROGRAM::LINKAGE_FAILED" << infoLog
+                  << std::endl;
+    } else {
         glUseProgram(programHandle);
     }
-    std::cout << vertexShader << " " << fragmentShader << " shader program: " << programHandle << std::endl;
+    std::cout << vertexShader << " " << fragmentShader
+              << " shader program: " << programHandle << std::endl;
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
-    
-    //glUseProgram(programHandle);
+
+    // glUseProgram(programHandle);
     return programHandle;
 }
--- src/shader.h
+++ src/shader.h
@@ -1,12 +1,10 @@
+#include <string>
 #include <vector>
+
 std::string read_shader_file(std::string &f);
 
-int use_shader(std::string &vrt_path,
-               std::string &frg_path,
-               std::vector<float> aXYZ,
-               std::vector<float> aNrm,
-               std::vector<float> aTex,
-               std::vector<unsigned int> aTri_XYZ);
+int use_shader(std::string &vrt_path, std::string &frg_path,
+               std::vector<float> aXYZ, std::vector<float> aNrm,
+               std::vector<float> aTex, std::vector<unsigned int> aTri_XYZ);
 
-int use_shader(std::string &vrt_path,
-               std::string &frg_path);
+int use_shader(std::string &vrt_path, std::string &frg_path);
